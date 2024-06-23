0f1249ae0f57556833d2e70b2743d8ae734a0da3
Mert <mertacu@hotmail.com>
2024-06-23 22:28:35 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import core.Helper;
+import view.LoginView;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        Helper.setTheme();
+
+        LoginView loginView = new LoginView();
+    }
+}
